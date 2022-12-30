7132874e76c74efd2a39ea23a87932f45e3593c6
Odejinmi Samuel <odejinmisamuel@gmail.com>
2022-12-30 09:27:01 +0100

    Update footer.blade.php
    added tawk.to

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -77,6 +77,21 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
             </div>
         </div>
     </footer>
+    
+    
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/63ae9f1bc2f1ac1e202ad663/1glh1r1du';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 
     <script src= {{ asset("/js/main.js") }}></script>
    
